af20ef1f2bb99ac817df5ab405c73a1324f06dfe
Akshay <101812659+Akshay03x@users.noreply.github.com>
2022-09-30 22:18:13 +0530

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -6,11 +6,9 @@ const Header=()=>{
     useEffect(()=>{
         fetch('http://localhost:3033/Faculty').then(res=>res.json().then(res=>setData(res)));
     },[])
-    // return <h1>hello</h1> 
     const Faculty=data.map((e)=>{
         return <h1>{e.FacultyName}</h1>;
     })
-
     return Faculty;
 }
 export default Header;
