6198b0342868692b7f9de344b1a371649af59f5c
Akshay <101812659+Akshay03x@users.noreply.github.com>
2022-11-14 21:14:21 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,5 +1,5 @@
-const express=require('express');
-const router=express.Router();
+const express=require('express')
+const router=express.Router()
 
 router.use('/user/:id',(req,res,next)=>{
     // res.send('hello from user');
